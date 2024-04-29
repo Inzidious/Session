49577c8b713afef4e9822ec4d751ca23fb8d24a4
Shawn <voshtt@gmail.com>
2024-04-28 21:16:44 -0700

    recent changes

---
+++ SessionApp/Views/BackupQuery.swift
@@ -0,0 +1,18 @@
+//
+//  BackupQuery.swift
+//  SessionApp
+//
+//  Created by Shawn McLean on 4/27/24.
+//
+
+import SwiftUI
+
+struct BackupQuery: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackupQuery()
+}
---
+++ SessionApp/Views/SessionHistory.swift
@@ -0,0 +1,18 @@
+//
+//  SessionHistory.swift
+//  SessionApp
+//
+//  Created by Shawn McLean on 4/28/24.
+//
+
+import SwiftUI
+
+struct SessionHistory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionHistory()
+}
